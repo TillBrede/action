a4b3f11ae2a66d6b1e12a3cd780096d5408a1e9b
TillBrede <51406030+TillBrede@users.noreply.github.com>
2021-01-08 10:00:46 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
-if [ -z {{inputs.phpunit-versio}}" ];
+if [ -z $1 ]
 then
-  wget https://phar.phpunit.de/phpunit-${{ inputs.phpunit-version }}.phar;
-else 
-  wget https://phar.phpunit.de/phpunit.phar
-if [ -z {{inputs.phpunit-versio}}" ];
-then
-  phpdbg -qrr phpunit.phar tests --configuration tests/phpunit.xml;
+  wget https://phar.phpunit.de/phpunit-$1.phar
 else
-  phpdbg -qrr phpunit-${{ inputs.phpunit-version }}.phar tests --configuration tests/phpunit.xml;
+  wget https://phar.phpunit.de/phpunit.phar
+fi
+
+if [ -z $1 ]
+then
+  phpdbg -qrr phpunit.phar tests --configuration tests/phpunit.xml
+else
+  phpdbg -qrr phpunit-$1.phar tests --configuration tests/phpunit.xml
+fi
